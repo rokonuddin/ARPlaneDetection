3f813e469a43bcdb5b31e4a8855e333ca2315d7f
Mohammed Rokon Uddin <rokon.uddin@sekai-lab-bd.com>
2018-01-23 14:28:47 +0600

    Initial Commit

---
+++ ARPlaneDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARPlaneDetection
+//
+//  Created by Mohammed Rokon Uddin on 23/1/18.
+//  Copyright © 2018 Mohammed Rokon Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
